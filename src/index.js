c98a3373252641afcc4b84285547c560b62ddcc2
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-03-17 11:35:11 +0530

    completed pub to mail

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ const app = express();
 
 const apiRoutes = require('./routes')
 
-const { ServerConfig, Logger } = require('./config');
+const { ServerConfig, Logger, Queue } = require('./config');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Sucessfully started the server on PORT: ${ServerConfig.PORT}`);
     Logger.info("Successfully started the server", "root", {});
+    await Queue.connectQueue();
+    console.log("queue connected")
 });
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes')
 const axios = require('axios');
 const { BookingRepository } = require('../repositories')
 const AppError = require('../utils/errors/app-errors')
-const { ServerConfig } = require('../config')
+const { ServerConfig, Queue } = require('../config')
 
 const db = require('../models')
 
@@ -12,9 +12,14 @@ async function createBooking(data) {
         const result = await db.sequelize.transaction(async function bookingImplementation(t) {
             const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`)
             const flightData = flight.data.data
-            if(data.noOfSeats > flightData.totalSeats) {
+            if (data.noOfSeats > flightData.totalSeats) {
                 throw new AppError('required number of seats not available', StatusCodes.BAD_REQUEST);
             }
+            Queue.sendData({
+                recepientEmail: 'amanjotsingh2309@gmail.com',
+                subject: 'Flight booked',
+                text: `Booking successfully done for the booking ${data.bookingId}`
+            });
             return true;
         });
     } catch (error) {
